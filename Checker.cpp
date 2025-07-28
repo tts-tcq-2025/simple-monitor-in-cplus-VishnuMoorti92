18cf20713eaf9197d17aed8faf8cde12d6143f08
VishnuMoorti92 <bhatvishnuy@gmail.com>
2025-07-28 23:19:58 +0530

    Update Checker.cpp
    Used function for print

--- Checker.cpp
+++ Checker.cpp
@@ -1,38 +1,50 @@
+
 #include <assert.h>
 #include <iostream>
+#include <string>
+#include <functional>
 using namespace std;
 
-bool isTemperatureOk(float temperature) {
+// Define a type alias for the message handler
+using MessageHandler = function<void(const string&)>;
+
+// Centralized print function
+void printMessage(const string& message) {
+  cout << message << endl;
+}
+
+bool isTemperatureOk(float temperature, MessageHandler handler) {
   if (temperature < 0 || temperature > 45) {
-    cout << "Temperature out of range!\n";
+    handler("Temperature out of range!");
     return false;
   }
   return true;
 }
 
-bool isSocOk(float soc) {
+bool isSocOk(float soc, MessageHandler handler) {
   if (soc < 20 || soc > 80) {
-    cout << "State of Charge out of range!\n";
+    handler("State of Charge out of range!");
     return false;
   }
   return true;
 }
 
-bool isChargeRateOk(float chargeRate) {
+bool isChargeRateOk(float chargeRate, MessageHandler handler) {
   if (chargeRate > 0.8) {
-    cout << "Charge Rate out of range!\n";
+    handler("Charge Rate out of range!");
     return false;
   }
   return true;
 }
 
-bool batteryIsOk(float temperature, float soc, float chargeRate) {
-  return isTemperatureOk(temperature) &&
-         isSocOk(soc) &&
-         isChargeRateOk(chargeRate);
+bool batteryIsOk(float temperature, float soc, float chargeRate, MessageHandler handler) {
+  return isTemperatureOk(temperature, handler) &&
+         isSocOk(soc, handler) &&
+         isChargeRateOk(chargeRate, handler);
 }
 
 int main() {
-  assert(batteryIsOk(25, 70, 0.7) == true);
-  assert(batteryIsOk(50, 85, 0) == false);
+  // You can replace printMessage with a lambda or another handler if needed
+  assert(batteryIsOk(25, 70, 0.7, printMessage) == true);
+  assert(batteryIsOk(50, 85, 0, printMessage) == false);
 }
